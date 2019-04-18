974dad0e1b25550081933fbd0c763d1911daaffd
gnd <gnd@itchybit.org>
2019-04-18 14:47:48 +0300

    adding more sources to doktorka.cz spider

--- comment_spiders/doktorka_run.sh
+++ comment_spiders/doktorka_run.sh
@@ -2,18 +2,83 @@
 #
 # run scrapy on doktorka.cz
 ###########################
-scrapy crawl doktorka_cz -a maxpages=12 -a start_url=https://diskuse.doktorka.cz/zase-rakovina
-scrapy crawl doktorka_cz -a maxpages=10 -a start_url=https://diskuse.doktorka.cz/rakovina-slinivky
-scrapy crawl doktorka_cz -a maxpages=6 -a start_url=https://diskuse.doktorka.cz/rakovina-mam-rok-zivota
-scrapy crawl doktorka_cz -a maxpages=3 -a start_url=https://diskuse.doktorka.cz/rakovina-delohy
-scrapy crawl doktorka_cz -a maxpages=4 -a start_url=https://diskuse.doktorka.cz/rakovina-krku
-scrapy crawl doktorka_cz -a maxpages=17 -a start_url=https://diskuse.doktorka.cz/rakovina-konecniku
-scrapy crawl doktorka_cz -a maxpages=4 -a start_url=https://diskuse.doktorka.cz/rakovina-plic/43506
-scrapy crawl doktorka_cz -a maxpages=23 -a start_url=https://diskuse.doktorka.cz/rakovina-delozniho-cipku
-scrapy crawl doktorka_cz -a maxpages=15 -a start_url=https://diskuse.doktorka.cz/rakovina-varlat
-scrapy crawl doktorka_cz -a maxpages=22 -a start_url=https://diskuse.doktorka.cz/rakovina-jater
-scrapy crawl doktorka_cz -a maxpages=18 -a start_url=https://diskuse.doktorka.cz/rakovina-prsu/36324
-scrapy crawl doktorka_cz -a maxpages=8 -a start_url=https://diskuse.doktorka.cz/rakovina
+# search keyword: rakovina
+scrapy crawl doktorka_cz -a maxpages=13 -a start_url=https://diskuse.doktorka.cz/zase-rakovina
+scrapy crawl doktorka_cz -a maxpages=2 -a start_url=https://doktorka.cz/diskuse/rakovina-mocoveho-mechyre
+scrapy crawl doktorka_cz -a maxpages=1 -a start_url=https://doktorka.cz/diskuse/artemisinin-rakovina
+scrapy crawl doktorka_cz -a maxpages=11 -a start_url=https://diskuse.doktorka.cz/rakovina-slinivky
+scrapy crawl doktorka_cz -a maxpages=3 -a start_url=https://doktorka.cz/diskuse/rakovina-je-nelecena-zloba
+scrapy crawl doktorka_cz -a maxpages=1 -a start_url=https://doktorka.cz/diskuse/mms-pripravek-a-rakovina-blog
+scrapy crawl doktorka_cz -a maxpages=1 -a start_url=https://doktorka.cz/diskuse/prokleta-rakovina-kdyby-neexistovala-jak-by-byl-svet-krasnejsi
+scrapy crawl doktorka_cz -a maxpages=7 -a start_url=https://diskuse.doktorka.cz/rakovina-mam-rok-zivota
+scrapy crawl doktorka_cz -a maxpages=1 -a start_url=https://doktorka.cz/diskuse/hemeroidy-nebo-rakovina-tlusteho-streva
+scrapy crawl doktorka_cz -a maxpages=2 -a start_url=https://doktorka.cz/diskuse/rakovina-tlusteho-streva
+scrapy crawl doktorka_cz -a maxpages=1 -a start_url=https://doktorka.cz/diskuse/rakovina-srdce
+scrapy crawl doktorka_cz -a maxpages=4 -a start_url=https://diskuse.doktorka.cz/rakovina-delohy
+scrapy crawl doktorka_cz -a maxpages=2 -a start_url=https://doktorka.cz/diskuse/rakovina-jicnu-zaludku
+scrapy crawl doktorka_cz -a maxpages=1 -a start_url=https://doktorka.cz/diskuse/rakovina-cervena-repa
+scrapy crawl doktorka_cz -a maxpages=2 -a start_url=https://doktorka.cz/diskuse/rakovina-delozniho-hrdla
+scrapy crawl doktorka_cz -a maxpages=1 -a start_url=https://doktorka.cz/diskuse/hemeroidy-rakovina
+scrapy crawl doktorka_cz -a maxpages=2 -a start_url=https://doktorka.cz/diskuse/rakovina-plic-priznaky-drobne-pichani
+scrapy crawl doktorka_cz -a maxpages=2 -a start_url=https://doktorka.cz/diskuse/rakovina-kuze-solarka
+scrapy crawl doktorka_cz -a maxpages=5 -a start_url=https://diskuse.doktorka.cz/rakovina-krku
+scrapy crawl doktorka_cz -a maxpages=1 -a start_url=https://doktorka.cz/diskuse/jaka-stadia-rakovina
+scrapy crawl doktorka_cz -a maxpages=18 -a start_url=https://diskuse.doktorka.cz/rakovina-konecniku
+scrapy crawl doktorka_cz -a maxpages=1 -a start_url=https://doktorka.cz/diskuse/rakovina/11742
+scrapy crawl doktorka_cz -a maxpages=3 -a start_url=https://doktorka.cz/diskuse/pozna-rakovina-rozboru-krve-krevniho
+scrapy crawl doktorka_cz -a maxpages=2 -a start_url=https://doktorka.cz/diskuse/rakovina-ust
+scrapy crawl doktorka_cz -a maxpages=5 -a start_url=https://diskuse.doktorka.cz/rakovina-plic/43506
+scrapy crawl doktorka_cz -a maxpages=1 -a start_url=https://doktorka.cz/diskuse/rakovina-krve-myelom-plasmocyt
+scrapy crawl doktorka_cz -a maxpages=1 -a start_url=https://doktorka.cz/diskuse/rakovina-kozni
+scrapy crawl doktorka_cz -a maxpages=3 -a start_url=https://doktorka.cz/diskuse/rakovina-prostaty
+scrapy crawl doktorka_cz -a maxpages=24 -a start_url=https://diskuse.doktorka.cz/rakovina-delozniho-cipku
+scrapy crawl doktorka_cz -a maxpages=16 -a start_url=https://diskuse.doktorka.cz/rakovina-varlat
+scrapy crawl doktorka_cz -a maxpages=23 -a start_url=https://diskuse.doktorka.cz/rakovina-jater
+scrapy crawl doktorka_cz -a maxpages=19 -a start_url=https://diskuse.doktorka.cz/rakovina-prsu/36324
+scrapy crawl doktorka_cz -a maxpages=9 -a start_url=https://diskuse.doktorka.cz/rakovina
+# search keyword: tumor
+scrapy crawl doktorka_cz -a maxpages=1 -a start_url=https://doktorka.cz/diskuse/tumor-papilom
+# search keyword: nador
+scrapy crawl doktorka_cz -a maxpages=1 -a start_url=https://doktorka.cz/diskuse/kdo-nadorove-onemocneni-pili-jste
+scrapy crawl doktorka_cz -a maxpages=2 -a start_url=https://doktorka.cz/diskuse/odstraneni-tukoveho-nadoru
+scrapy crawl doktorka_cz -a maxpages=5 -a start_url=https://doktorka.cz/diskuse/mozkove-nadory
+scrapy crawl doktorka_cz -a maxpages=10 -a start_url=https://doktorka.cz/diskuse/metoda-mudr-jurkovice-blokada-metabolimu
+scrapy crawl doktorka_cz -a maxpages=1 -a start_url=https://doktorka.cz/diskuse/nador
+scrapy crawl doktorka_cz -a maxpages=3 -a start_url=https://doktorka.cz/diskuse/jak-poznat-nador
+scrapy crawl doktorka_cz -a maxpages=4 -a start_url=https://doktorka.cz/diskuse/ockovani-proti-nadoru-delozniho-cipku
+scrapy crawl doktorka_cz -a maxpages=1 -a start_url=https://doktorka.cz/diskuse/nador-vajecnicich
+scrapy crawl doktorka_cz -a maxpages=4 -a start_url=https://doktorka.cz/diskuse/nezhoubny-nador-jater
+scrapy crawl doktorka_cz -a maxpages=1 -a start_url=https://doktorka.cz/diskuse/vyrustky-pod-kuzi-nadory/10766
+scrapy crawl doktorka_cz -a maxpages=37 -a start_url=https://doktorka.cz/diskuse/mam-nador-mozku
+scrapy crawl doktorka_cz -a maxpages=1 -a start_url=https://doktorka.cz/diskuse/podozrenie-nador
+scrapy crawl doktorka_cz -a maxpages=2 -a start_url=https://doktorka.cz/diskuse/nador-prsu
+# search keyword: melanom
+scrapy crawl doktorka_cz -a maxpages=2 -a start_url=https://doktorka.cz/diskuse/maligny-melanom
+# search keyword: onkolog
+scrapy crawl doktorka_cz -a maxpages=1 -a start_url=https://doktorka.cz/diskuse/onkologicke-onemocneni-dovolena
+# search keyword: karci
+scrapy crawl doktorka_cz -a maxpages=1 -a start_url=https://doktorka.cz/diskuse/dieta-operaci-karcinomu-strev
+# search keyword: chemoter
+scrapy crawl doktorka_cz -a maxpages=1 -a start_url=https://doktorka.cz/diskuse/chemoterapie
+scrapy crawl doktorka_cz -a maxpages=2 -a start_url=https://doktorka.cz/diskuse/chemoterapie-jidla
+scrapy crawl doktorka_cz -a maxpages=64 -a start_url=https://doktorka.cz/diskuse/lecba-rakoviny-bez-chemoterapie-pomoci
+scrapy crawl doktorka_cz -a maxpages=2 -a start_url=https://doktorka.cz/diskuse/chemoterapie-ozarovani
+# search keyword: rakovin
+scrapy crawl doktorka_cz -a maxpages=3 -a start_url=https://doktorka.cz/diskuse/black-salve-mast-na-rakovinu
+scrapy crawl doktorka_cz -a maxpages=3 -a start_url=https://doktorka.cz/diskuse/pravidelny-klystyr-je-prevenci-rakoviny-strev
+scrapy crawl doktorka_cz -a maxpages=5 -a start_url=https://doktorka.cz/diskuse/antiperspiranty-zpusobuji-rakovinu-prsu
+scrapy crawl doktorka_cz -a maxpages=1 -a start_url=https://doktorka.cz/diskuse/nova-era-oblasti-leceni
+scrapy crawl doktorka_cz -a maxpages=2 -a start_url=https://doktorka.cz/diskuse/vyleceni-predrakovinovych-zmen-na-deloznim-cipku-zkusenosti
+scrapy crawl doktorka_cz -a maxpages=2 -a start_url=https://doktorka.cz/diskuse/rostlina-z-amazonskeho-pralesu-proti-rakovine-i-hiv
+scrapy crawl doktorka_cz -a maxpages=2 -a start_url=https://doktorka.cz/diskuse/mam-rakovinu
+scrapy crawl doktorka_cz -a maxpages=3 -a start_url=https://doktorka.cz/diskuse/ockovani-proti-rakovine-delozniho-cipku
+scrapy crawl doktorka_cz -a maxpages=34 -a start_url=https://doktorka.cz/diskuse/lecba-rakoviny-javorovy-sirup-soda
+scrapy crawl doktorka_cz -a maxpages=1 -a start_url=https://doktorka.cz/diskuse/avemar-proti-rakovine
+scrapy crawl doktorka_cz -a maxpages=1 -a start_url=https://doktorka.cz/diskuse/priciny-rakoviny
+scrapy crawl doktorka_cz -a maxpages=1 -a start_url=https://doktorka.cz/diskuse/rakovinotvorne-polypy-mocovem-mechyri
+scrapy crawl doktorka_cz -a maxpages=1 -a start_url=https://doktorka.cz/diskuse/vyziva-pri-rakovine-jicnu
+scrapy crawl doktorka_cz -a maxpages=1 -a start_url=https://doktorka.cz/diskuse/strava-pri-rakovine-hrubeho-creva
+scrapy crawl doktorka_cz -a maxpages=4 -a start_url=https://doktorka.cz/diskuse/ukrain-leci-rakovinu
 
 # output everything into doktorka.in
 python flush_comments.py 'https://diskuse.doktorka.cz' doktorka.in
